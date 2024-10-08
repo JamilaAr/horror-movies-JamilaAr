17c0ce7a3dafa74073c07c85929e1030254ab90d
jamila <jamilaarabat93@gmail.com>
2024-10-07 20:32:38 -0400

    Well Done!

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,6 +1,7 @@
-SELECT id AS "Movie_ID", name AS "Movie_Title", imdb_rating AS "Rating"
+
+SELECT id AS 'Movie_ID', name AS 'Movie_Title', imdb_rating AS 'Rating'
 FROM movies
 WHERE year <= 1985
-AND genre = "horror"
+AND genre = 'horror'
 ORDER BY imdb_rating DESC
 LIMIT 3
